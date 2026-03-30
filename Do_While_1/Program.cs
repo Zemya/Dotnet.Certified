6682bb76a6771e660fd58893e79080ca8ecc1f68
Cole D. <79370488+Zemya@users.noreply.github.com>
2026-03-31 07:59:03 +0800

    Add files via upload

---
+++ Do_While_1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string? input;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    input = Console.ReadLine();
+    if (input != null)
+    {
+        validNumber = int.TryParse(input, out numValue);
+        if (validNumber)
+        {
+            if (numValue >= 5 && numValue <= 10)
+            {
+                valueEntered = input;
+                Console.WriteLine($"You entered ({input}), a valid number");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, you entered ({input}) an invalid number, please try again");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        }
+    }
+}
+
+while (input == null);
---
+++ Do_While_Challenge/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Random random = new Random();
+
+int hero = 10;
+int monster = 10;
+
+do
+{
+    int heroAttack = random.Next(1, 11);
+    int monsterAttack = random.Next(1, 11);
+
+    monster -= heroAttack;
+    hero -= monsterAttack;
+
+    Console.WriteLine($"Hero attacks for {heroAttack}. Monster health: {monster}");
+    if (monster <= 0)
+    {
+        Console.WriteLine("Monster defeated! Hero wins!");
+        break;
+    }
+
+    Console.WriteLine($"Monster attacks for {monsterAttack}. Hero health: {hero}");
+    if (hero <= 0)
+    {
+        Console.WriteLine("Hero defeated! Monster wins!");
+        break;
+    }
+
+    Console.WriteLine();
+
+} while (hero > 0 && monster > 0);
