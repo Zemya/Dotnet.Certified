ec14810a24bf6fb4633a4c46b109a877a1c6f3ac
Cole D. <79370488+Zemya@users.noreply.github.com>
2026-03-31 07:21:23 +0800

    Add files via upload

---
+++ Boolean_Expressions/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string permission = "Admin||Manager";
+int level = 60;
+
+if (permission == "Admin" && level > 55)
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission == "Admin" && level <= 55)
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission == "Manager" && level > 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if (permission == "Manager" && level < 20)
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+else if (permission != "Admin || Manager")
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
