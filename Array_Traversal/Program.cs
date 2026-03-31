b1cd18e01f12c278a9bd6ab4c4a9e4514f9fdf04
Cole D. <79370488+Zemya@users.noreply.github.com>
2026-03-31 08:13:21 +0800

    Add files via upload

---
+++ Array_Traversal/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Array Creation and Traversal
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+
+int sum = 0;
+int bin = 0;
+
+Console.WriteLine("\n");
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.\n");
